1c1a9fe2b8a254d623cc78f2b01a02a8f8300e91
hs282 <happyfeller29@ymail.com>
2025-01-31 18:11:57 -0500

    set up Node.js backend

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from the backend!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
